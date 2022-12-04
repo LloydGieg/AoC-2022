b78cf271970c7718d728fddf456cc1ba061552d1
Lloyd Gieg <lloyd@gieg.net>
2022-12-03 23:41:17 -0600

    Added Day 4

--- aoc2022/aoc2022d4.py
+++ aoc2022/aoc2022d4.py
@@ -11,21 +11,42 @@ def initdf(infile):
 
     Create a DataFrame from a text file.
     """
-    return infile
+    inlist = []
+    with open(infile, 'r') as f:
+        for x in f.read().split('\n'):
+            thisline = []
+            for y in x.split(','):
+                for z in y.split('-'):
+                    thisline.append(int(z))
+            inlist.append(thisline)
+    return pandas.DataFrame(inlist)
 
 
 def p1(indf):
     """AoC 2022 Day 4 Part 1
 
+    Find overlapping sections in a DataFrame.
     """
-    return indf
+    overlaps = 0
+    for x in zip(indf[0], indf[1], indf[2], indf[3]):
+        y, z = [set(range(x[0], x[1] + 1)), set(range(x[2], x[3] + 1))]
+        smol = min([len(y), len(z)])
+        if len(y & z) == smol:
+            overlaps += 1
+    return overlaps
 
 
 def p2(indf):
     """AoC 2022 Day 4 Part 2
 
+    Find overlapping sections in a DataFrame.
     """
-    return indf
+    overlaps = 0
+    for x in zip(indf[0], indf[1], indf[2], indf[3]):
+        y, z = [set(range(x[0], x[1] + 1)), set(range(x[2], x[3] + 1))]
+        if len(y & z) >= 1:
+            overlaps += 1
+    return overlaps
 
 
 if __name__ == '__main__':

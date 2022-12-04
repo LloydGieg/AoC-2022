7adee751a2956b5a1b0439e4429e2749deb14b6b
Lloyd Gieg <lloyd@gieg.net>
2022-12-04 01:11:13 -0600

    Added Day 5

---
+++ aoc2022/aoc2022d5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import pandas
+
+day = 5
+inputdir = '../InputFiles'
+
+
+def initdf(infile):
+    """AoC 2022 Day 5 Init
+
+    Create a DataFrame from a text file.
+    """
+    return infile
+
+
+def p1(indf):
+    """AoC 2022 Day 5 Part 1
+
+    Find overlapping sections in a DataFrame.
+    """
+    return indf
+
+
+def p2(indf):
+    """AoC 2022 Day 5 Part 2
+
+    Find overlapping sections in a DataFrame.
+    """
+    return indf
+
+
+if __name__ == '__main__':
+    df = initdf(f"{inputdir}/d{day}.txt")
+
+    for i in range(2):
+        j = globals()[f"p{i + 1}"]
+        print(f"Day {day} Part {i + 1}: {j(df)}")

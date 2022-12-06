712dede73e8acd68253fa2da312c406d85391814
Lloyd Gieg <lloyd@gieg.net>
2022-12-05 23:36:36 -0600

    Completed Day 6

--- aoc2022/aoc2022d6.py
+++ aoc2022/aoc2022d6.py
@@ -11,21 +11,32 @@ def initdf(infile):
 
     Create a DataFrame from a text file.
     """
-    return infile
+    with open(infile, 'r') as f:
+        return pandas.DataFrame([f.readline()])
 
 
 def p1(indf):
     """AoC 2022 Day 6 Part 1
 
+    Find the first unique 4-character substring in a string
     """
-    return indf
+    indata = indf[0][0]
+    inlist = [indata[x:x + 4] for x in range(len(indata) - 4)]
+    for x in inlist:
+        if len(set(x)) == 4:
+            return str(inlist.index(x) + 4)
 
 
 def p2(indf):
     """AoC 2022 Day 6 Part 2
 
+    Find the first unique 14-character substring in a string
     """
-    return indf
+    indata = indf[0][0]
+    inlist = [indata[x:x + 14] for x in range(len(indata) - 14)]
+    for x in inlist:
+        if len(set(x)) == 14:
+            return str(inlist.index(x) + 14)
 
 
 if __name__ == '__main__':

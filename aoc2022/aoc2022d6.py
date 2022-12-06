6ac9a4cc2d3ddf48ce548572fa5a6bfb8d2b6dff
Lloyd Gieg <lloyd@gieg.net>
2022-12-05 23:42:12 -0600

    Minor optimizations for Day 6

--- aoc2022/aoc2022d6.py
+++ aoc2022/aoc2022d6.py
@@ -20,11 +20,12 @@ def p1(indf):
 
     Find the first unique 4-character substring in a string
     """
+    chars = 4
     indata = indf[0][0]
-    inlist = [indata[x:x + 4] for x in range(len(indata) - 4)]
+    inlist = [indata[x:x + chars] for x in range(len(indata) - chars)]
     for x in inlist:
-        if len(set(x)) == 4:
-            return str(inlist.index(x) + 4)
+        if len(set(x)) == chars:
+            return str(inlist.index(x) + chars)
 
 
 def p2(indf):
@@ -32,11 +33,12 @@ def p2(indf):
 
     Find the first unique 14-character substring in a string
     """
+    chars = 14
     indata = indf[0][0]
-    inlist = [indata[x:x + 14] for x in range(len(indata) - 14)]
+    inlist = [indata[x:x + chars] for x in range(len(indata) - chars)]
     for x in inlist:
-        if len(set(x)) == 14:
-            return str(inlist.index(x) + 14)
+        if len(set(x)) == chars:
+            return str(inlist.index(x) + chars)
 
 
 if __name__ == '__main__':

9ca081022ee1fa0e6da7f08b7759ea06f52df981
Lloyd Gieg <lloyd@gieg.net>
2022-12-12 16:39:17 -0600

    Completed Day 12

--- aoc2022/aoc2022d12.py
+++ aoc2022/aoc2022d12.py
@@ -5,27 +5,180 @@ import pandas
 day = 12
 inputdir = '../InputFiles'
 
+inlist = []
+start = []
+end = []
+djikstra = []
+
 
 def initdf(infile):
     """AoC 2022 Day 12 Init
 
     Create a DataFrame from a text file.
     """
-    return infile
+    global inlist
+    outlist = []
+    with open(infile, 'r') as f:
+        for x in f.read().split('\n'):
+            outlist.append([y for y in x])
+    return pandas.DataFrame(outlist)
+
+
+def initinlist(indf):
+    global inlist, start, end
+
+    alphavalues = {chr(x + 96): x for x in range(1, 27)}
+
+    inlist = indf.T.values.tolist()
+
+    for x in range(len(inlist)):
+        for y in range(len(inlist[x])):
+            if inlist[x][y] == 'S':
+                start = [x, y]
+                inlist[x][y] = alphavalues['a']
+            elif inlist[x][y] == 'E':
+                end = [x, y]
+                inlist[x][y] = alphavalues['z']
+            else:
+                inlist[x][y] = alphavalues[inlist[x][y]]
+
+
+def checkpos(x, y):
+    global inlist, djikstra
+    adjacents = []
+    if x > 0:
+        if inlist[x][y] - inlist[x-1][y] <= 1:
+            if djikstra[x-1][y] != '':
+                adjacents.append(djikstra[x-1][y])
+    if x < len(djikstra)-1:
+        if inlist[x][y] - inlist[x+1][y] <= 1:
+            if djikstra[x+1][y] != '':
+                adjacents.append(djikstra[x+1][y])
+    if y > 0:
+        if inlist[x][y] - inlist[x][y-1] <= 1:
+            if djikstra[x][y-1] != '':
+                adjacents.append(djikstra[x][y-1])
+    if y < len(djikstra[x])-1:
+        if inlist[x][y] - inlist[x][y+1] <= 1:
+            if djikstra[x][y+1] != '':
+                adjacents.append(djikstra[x][y+1])
+    return adjacents
 
 
 def p1(indf):
     """AoC 2022 Day 12 Part 1
 
+    Find the distance from a single start point to a single end point
     """
-    return indf
+    global inlist, djikstra, start, end
+
+    initinlist(indf)
+
+    djikstra = []
+    unvisited = set()
+    for x in range(len(inlist)):
+        djikstra.append([])
+        for y in range(len(inlist[x])):
+            djikstra[x].append('')
+            unvisited.add(f"{x},{y}")
+
+    djikstra[start[0]][start[1]] = 0
+    unvisited.remove(f"{start[0]},{start[1]}")
+
+    while len(unvisited) > 0:
+        tempvisited = unvisited.copy()
+        changed = 0
+        for z in sorted(tempvisited):
+            x, y = z.split(',')
+            x = int(x)
+            y = int(y)
+            adjacents = []
+            if x > 0:
+                if inlist[x][y] - inlist[x - 1][y] <= 1:
+                    if djikstra[x - 1][y] != '':
+                        adjacents.append(djikstra[x - 1][y])
+            if x < len(djikstra) - 1:
+                if inlist[x][y] - inlist[x + 1][y] <= 1:
+                    if djikstra[x + 1][y] != '':
+                        adjacents.append(djikstra[x + 1][y])
+            if y > 0:
+                if inlist[x][y] - inlist[x][y - 1] <= 1:
+                    if djikstra[x][y - 1] != '':
+                        adjacents.append(djikstra[x][y - 1])
+            if y < len(djikstra[x]) - 1:
+                if inlist[x][y] - inlist[x][y + 1] <= 1:
+                    if djikstra[x][y + 1] != '':
+                        adjacents.append(djikstra[x][y + 1])
+            if len(adjacents) > 0:
+                changed += 1
+                djikstra[x][y] = min(adjacents) + 1
+                unvisited.remove(z)
+        if changed == 0:
+            break
+
+    return djikstra[end[0]][end[1]] or 999
 
 
 def p2(indf):
     """AoC 2022 Day 12 Part 2
 
+    Find the lowest distance from multiple start points to a single end point
     """
-    return indf
+    global inlist, djikstra, start, end
+
+    initinlist(indf)
+    start = end
+
+    djikstra = []
+    unvisited = set()
+    for x in range(len(inlist)):
+        djikstra.append([])
+        for y in range(len(inlist[x])):
+            djikstra[x].append('')
+            unvisited.add(f"{x},{y}")
+
+    djikstra[start[0]][start[1]] = 0
+    unvisited.remove(f"{start[0]},{start[1]}")
+
+    while len(unvisited) > 0:
+        tempvisited = unvisited.copy()
+        changed = 0
+        for z in sorted(tempvisited):
+            x, y = z.split(',')
+            x = int(x)
+            y = int(y)
+            adjacents = []
+            if x > 0:
+                if inlist[x][y] - inlist[x - 1][y] >= -1:
+                    if djikstra[x - 1][y] != '':
+                        adjacents.append(djikstra[x - 1][y])
+            if x < len(djikstra) - 1:
+                if inlist[x][y] - inlist[x + 1][y] >= -1:
+                    if djikstra[x + 1][y] != '':
+                        adjacents.append(djikstra[x + 1][y])
+            if y > 0:
+                if inlist[x][y] - inlist[x][y - 1] >= -1:
+                    if djikstra[x][y - 1] != '':
+                        adjacents.append(djikstra[x][y - 1])
+            if y < len(djikstra[x]) - 1:
+                if inlist[x][y] - inlist[x][y + 1] >= -1:
+                    if djikstra[x][y + 1] != '':
+                        adjacents.append(djikstra[x][y + 1])
+            if len(adjacents) > 0:
+                changed += 1
+                djikstra[x][y] = min(adjacents) + 1
+                unvisited.remove(z)
+        if changed == 0:
+            break
+
+    lowpoints = []
+    for x in range(len(inlist)):
+        for y in range(len(inlist[x])):
+            if inlist[x][y] == 1:
+                # print(f"{x},{y} is {djikstra[x][y]}")
+                if djikstra[x][y]:
+                    lowpoints.append(djikstra[x][y])
+    return min(lowpoints)
 
 
 if __name__ == '__main__':

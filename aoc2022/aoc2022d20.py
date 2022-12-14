a800f41a4bde56916473e85dd5c405c9e7599ebf
Lloyd Gieg <lloyd@gieg.net>
2022-12-13 20:06:59 -0600

    Prepared days 14 - 25

---
+++ aoc2022/aoc2022d20.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pandas
+
+day = 20
+inputdir = '../InputFiles'
+
+
+def initdf(infile):
+    """AoC 2022 Day 20 Init
+
+    Create a DataFrame from a text file.
+    """
+    return infile
+
+
+def p1(indf):
+    """AoC 2022 Day 20 Part 1
+
+    """
+    return indf
+
+
+def p2(indf):
+    """AoC 2022 Day 20 Part 2
+
+    """
+    return indf
+
+
+if __name__ == '__main__':
+    df = initdf(f"{inputdir}/d{day}.txt")
+
+    for i in range(2):
+        j = globals()[f"p{i + 1}"]
+        print(f"Day {day} Part {i + 1}: {j(df)}")

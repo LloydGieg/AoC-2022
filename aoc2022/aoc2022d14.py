be8be130b587d7fe24f4f21676fec3ee18fc467c
Lloyd Gieg <lloyd@gieg.net>
2022-12-18 14:50:24 -0600

    Day 14 Part 2 optimizations

--- aoc2022/aoc2022d14.py
+++ aoc2022/aoc2022d14.py
@@ -73,32 +73,33 @@ def p2(indf):
     stuff = set()
     wstart = indf['start'].values.tolist()
     wend = indf['end'].values.tolist()
+    inc = 1000
     for x in range(len(wstart)):
         if wstart[x][0] == wend[x][0]:
             for y in range(min(wstart[x][1], wend[x][1]), max(wstart[x][1], wend[x][1]) + 1):
-                stuff.add(f"{wstart[x][0]},{y}")
+                stuff.add((wstart[x][0] * inc) + y)
             continue
         if wstart[x][1] == wend[x][1]:
             for y in range(min(wstart[x][0], wend[x][0]), max(wstart[x][0], wend[x][0]) + 1):
-                stuff.add(f"{y},{wstart[x][1]}")
+                stuff.add((y * inc) + wstart[x][1])
             continue
-    maxwall = max([int(x.split(',')[1]) for x in stuff]) + 2
+    maxwall = max([divmod(x, inc)[1] for x in stuff]) + 2
     counter = 0
     full = 0
     while full == 0:
         counter += 1
         sandloc = [500, 0]
         while 1:
-            if '499,1' in stuff and '500,1' in stuff and '501,1' in stuff:
+            if ((499 * inc) + 1) in stuff and ((500 * inc) + 1) in stuff and ((501 * inc) + 1) in stuff:
                 full = 1
                 break
             if sandloc[1] + 1 == maxwall:
-                stuff.add(f"{sandloc[0]},{sandloc[1]}")
+                stuff.add((sandloc[0] * inc) + sandloc[1])
                 break
-            if f"{sandloc[0]},{sandloc[1] + 1}" in stuff:
-                if f"{sandloc[0] - 1},{sandloc[1] + 1}" in stuff:
-                    if f"{sandloc[0] + 1},{sandloc[1] + 1}" in stuff:
-                        stuff.add(f"{sandloc[0]},{sandloc[1]}")
+            if ((sandloc[0] * inc) + sandloc[1]) + 1 in stuff:
+                if ((sandloc[0] - 1) * inc) + (sandloc[1] + 1) in stuff:
+                    if ((sandloc[0] + 1) * inc) + (sandloc[1] + 1) in stuff:
+                        stuff.add((sandloc[0] * inc) + sandloc[1])
                         break
                     else:
                         sandloc[0] += 1
